31357815aac70e583656b85cec9a7adabba9430e
davidbarrerot-source <david.barrerot@uqvirtual.edu.co>
2026-03-23 19:49:06 -0500

    Update Cliente.java

--- bancov1/Banco/src/main/java/Model/Cliente.java
+++ bancov1/Banco/src/main/java/Model/Cliente.java
@@ -1,62 +1,18 @@
 package Model;
 
-public class Cliente {
-    private String nombre;
-    private String apellido;
-    private String cedula;
-    private String direccion;
-    private String email;
-    private Banco banco;
-    private Cuenta cuenta ;
 
-    public Cliente(String nombre, String apellido, String cedula, String email, String direccion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.cedula = cedula;
-        this.email = email;
-        this.direccion = direccion;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
+   
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    package Model;
 
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+public record Cliente(
+            String nombre,
+            String apellido,
+            String cedula,
+            String direccion,
+            String email
+    ) {
     @Override
     public String toString() {
         return "Cliente{" +
